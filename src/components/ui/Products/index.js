500148be103a19bf894ab7b85266b702e2581c02
qhuy135520 <114981045+qhuy135520@users.noreply.github.com>
2025-09-06 13:35:35 +0700

    Delete src/components/ui/Products directory

--- src/components/ui/Products/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ProductsList } from "@/components/ui/products/ProductsList";
-export { default as ProductsRandom } from "@/components/ui/products/ProductsRandom";
-export { default as ProductsSlider } from "@/components/ui/products/ProductsSlider";
-
-export * as ProductListStyled from "@/components/ui/products/ProductsList.styled";
-export * as ProductsSliderStyled from "@/components/ui/products/ProductsSlider.styled";
