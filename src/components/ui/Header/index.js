0ee05720e97d8894c6b3b2823683c5b3e7e1a1b8
qhuy135520 <114981045+qhuy135520@users.noreply.github.com>
2025-09-06 13:35:24 +0700

    Delete src/components/ui/Header directory

--- src/components/ui/Header/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "@/components/ui/header/Header";
-export { default as LanguageSwitcher } from "@/components/ui/header/LanguageSwitcher";
-
-export * as HeaderStyled from "@/components/ui/header/Header.styled";
