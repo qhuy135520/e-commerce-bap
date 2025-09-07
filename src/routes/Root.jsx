477ce1ce6437993622f186f75c39e68a73125e5f
qhuy135520 <114981045+qhuy135520@users.noreply.github.com>
2025-09-07 21:06:31 +0700

    Update Root.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,8 +2,8 @@ import { Routes } from "react-router-dom";
 import { Suspense } from "react";
 
 import PrivateRoutes from "@/routes/configs/_private";
-import GlobalRoutes from "@/routes/configs/_public";
-import PublicRoutes from "@/routes/configs/_global";
+import GlobalRoutes from "@/routes/configs/_global";
+import PublicRoutes from "@/routes/configs/_public";
 
 import { Loading } from "@/components";
 
