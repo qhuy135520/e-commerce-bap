56919ffda03f897328b5279d69163870cb858964
Anhthuan25022003 <thuanit25223@gmai.com>
2025-09-11 10:31:12 +0700

    2025-09-11 fix feature review

--- src/components/ui/orderHistory/OrderItemInfo.jsx
+++ src/components/ui/orderHistory/OrderItemInfo.jsx
@@ -7,9 +7,10 @@ import { OrderHistoryTableStyled as OHTS, ReviewModal } from "@/components";
 
 import { formatCurrency } from "@/utils/helpers";
 
-export default function OrderItemInfo({ vendorProduct }) {
+export default function OrderItemInfo({ vendorProduct, orderStatus }) {
   const { isModalOpen, selectedProduct, openReviewModal, closeReviewModal, handleSubmitReview, loading, t } =
     useReview();
+  console.log("producttt", vendorProduct);
   return (
     <>
       {vendorProduct.map((product, index) => (
@@ -21,7 +22,11 @@ export default function OrderItemInfo({ vendorProduct }) {
               <p>x{product.quantity}</p>
               <OHTS.Price>{formatCurrency(product.price)}</OHTS.Price>
             </OHTS.InfoItem>
-            <Button size="middle" onClick={() => openReviewModal(product)} disabled={product.isReviewed}>
+            <Button
+              size="middle"
+              onClick={() => openReviewModal(product)}
+              disabled={product.isReviewed || orderStatus !== "completed"} // chỉ enable khi completed và chưa review
+            >
               {product.isReviewed ? t("order.review.reviewed") : t("order.review.review")}
             </Button>
           </OHTS.ItemLeft>
