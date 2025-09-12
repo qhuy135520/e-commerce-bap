72f8a1cdbf2be095bce433ebd7b4a920ea3fd2c4
Anhthuan25022003 <thuanit25223@gmai.com>
2025-09-12 14:53:04 +0700

    2025-09-12 fix url

--- src/pages/privatePages/admin/AdminManagerProductPage.jsx
+++ src/pages/privatePages/admin/AdminManagerProductPage.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { productsSelector } from "@/stores/rootSelector";
 import { productsThunk } from "@/stores/rootThunk";
-
-import { AdminManagerProductTable, AdminManagerProductHeader } from "@/components";
+import AdminManagerProductHeader from "@/components/ui/admin/AdminManagerProductHeader";
+import AdminManagerProductTable from "@/components/ui/admin/AdminManagerProductTable";
 
 export default function AdminManagerProductPage() {
   const dispatch = useDispatch();
