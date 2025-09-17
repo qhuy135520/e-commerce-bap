6cf084c6fd2102a259c5b8539811e25576b1a355
qhuy135520 <huyltqse135@gmail.com>
2025-09-17 11:05:27 +0700

    2025-09-17-fix: prevent locked user

--- src/pages/globalPages/home/HomePage.jsx
+++ src/pages/globalPages/home/HomePage.jsx
@@ -12,13 +12,24 @@ import {
 } from "@/components";
 
 import useProducts from "@/hooks/products/useProducts";
+import { useUser } from "@/hooks/authentication/useUser";
+import { useLogout } from "@/hooks/authentication/useLogout";
+import toast from "react-hot-toast";
 
 export default function HomePage() {
   const { error, fetchDataProducts, vendorId, isLoading } = useProducts();
+  const { user } = useUser();
+  const { logout } = useLogout();
 
   useEffect(() => {
+    if (user?.id) {
+      if (user.status === "inactive") {
+        logout();
+        toast.error("Tài khoản của bạn đã bị vô hiệu hóa. Vui lòng liên hệ quản trị viên.");
+      }
+    }
     fetchDataProducts();
-  }, [vendorId]);
+  }, [vendorId, user]);
 
   return (
     <Loading isLoading={isLoading} error={error}>
